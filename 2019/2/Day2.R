7db1d38d0dbbf5c1a0c7acdac0a1a64bc2f51798
Vi Thanh Pham <tran.vi.thanh.pham@gmail.com>
2019-12-04 23:16:16 +0100

    Day 2

---
+++ 2019/2/Day2.R
@@ -0,0 +1,34 @@
+# ============================================================================ #
+# Topic : Advent of Code
+# --- Day 2: 1202 Program Alarm ---
+# ============================================================================ #
+setwd("~/Advent of Code/2019/2")
+input <- as.vector(read.csv("input.txt", header = F), mode = "numeric")
+Intcode <- function(input, initialize) {
+  input[2] <- initialize[1]
+  input[3] <- initialize[2]
+  opcode <- input[1]
+  i = 1
+  while (opcode != 99) {
+    pos1 <- input[i + 1] + 1
+    pos2 <- input[i + 2] + 1
+    pos3 <- input[i + 3] + 1
+    if (opcode == 1) {
+      input[pos3] <- input[pos1] + input[pos2]
+    } else if (opcode == 2) {
+      input[pos3] <- input[pos1] * input[pos2]
+    } else if (opcode != 1 & opcode !=2) {stop("Something went wrong")}
+    i <- i + 4
+    opcode <- input[i]
+  }
+  return(input[1])
+}
+Intcode(input, c(12, 2))
+
+# ============================================================================ #
+# --- Part Two ---
+# ============================================================================ #
+for(i in 1:99) {
+  for (j in 1:99)
+    if (Intcode(input, c(i, j)) == 19690720) print(100 * i + j)
+}
